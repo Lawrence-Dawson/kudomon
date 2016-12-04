8865c50ab60af9217c0b2630eb0772d609e34cfa
Lawrence Dawson <lawrencedawson@hotmail.co.uk>
2016-12-04 15:42:08 +0000

    added new test

--- spec/features/kudomon_feature_spec.rb
+++ spec/features/kudomon_feature_spec.rb
@@ -2,12 +2,21 @@ require 'rails_helper'
 
 feature 'kudomon' do
 
+  context 'Welcome' do
+    scenario 'should display welcome message' do
+      visit '/kudomon'
+      expect(page).to have_content 'Welcome to Kudomon!'
+    end
+  end
+
   context 'kudomon environment' do
     scenario 'should display all kudomon' do
       visit '/kudomon'
-      expect(page).to have_content 'All local Kudomon'
+      expect(page).to have_content 'All Kudomon Sourbulb Mancharred Chikapu Whirtle Stoner Twomew'
     end
   end
+
+
 
 
 end
