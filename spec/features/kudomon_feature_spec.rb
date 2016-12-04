fadaf3dcf5ad066e9176379061ab74665e9365b4
Lawrence Dawson <lawrencedawson@hotmail.co.uk>
2016-12-04 15:49:01 +0000

    updated test requirements for smaller step by step development

--- spec/features/kudomon_feature_spec.rb
+++ spec/features/kudomon_feature_spec.rb
@@ -10,13 +10,21 @@ feature 'kudomon' do
   end
 
   context 'kudomon environment' do
-    scenario 'should display all kudomon' do
+    scenario 'should display no kudomon' do
       visit '/kudomon'
-      expect(page).to have_content 'All Kudomon Sourbulb Mancharred Chikapu Whirtle Stoner Twomew'
+      expect(page).to have_content 'No Kudomon in environment'
     end
   end
 
-
-
+  context 'Kudomon have been added' do
+  before do
+    Kudomon.create(name: 'Sourbulb')
+  end
+    scenario 'display kudomon' do
+      visit '/kudomon'
+      expect(page).to have_content 'All Kudomon Sourbulb'
+    end
+  end
 
 end
+#Mancharred Chikapu Whirtle Stoner Twomew
